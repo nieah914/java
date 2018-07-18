d9ae1963e1ffaa91064699f4c592b60a41923494
nieah <nieah@DESKTOP-P94D4VG>
2018-07-19 01:22:58 +0900

    complete

--- src/java_algorithm/DP_Back10844.java
+++ src/java_algorithm/DP_Back10844.java
@@ -5,29 +5,27 @@ import java.util.Scanner;
 
 public class DP_Back10844 {
 	public static int N;
-	
-	public static long [][]memorization;
-	public static long mod = 1000000000;
-	public static long result;
-	/*
+	public static int [][]matrix;
+	public static int [][]memorization;
+	public static int mod = 1000000000;
+	public static int result;
 	public static void main(String[] args)  {
 		Scanner sn = new Scanner(System.in);
 		N = sn.nextInt();
 		
-		memorization = new long[N+1][10];
-		for(int i = 1; i < N+1 ; i++) {
+		memorization = new int[N+1][10];
+		for(int i = 0; i < N+1 ; i++) {
 			Arrays.fill(memorization[i], -1);
 		}
 		result = 0;
 		for(int i = 0; i < 10 ; i++) {
-			result += (Dynamic(N,i) % 1000000000); 
+			result += Dynamic(N,i); 
 		}
-		
-		System.out.println(result % 1000000000);
+		result %= 1000000000;
+		System.out.println(result);
 	}
-	*/
 	
-	public static long Dynamic(int n, int l) {
+	public static int Dynamic(int n, int l) {
 		
 		
 		
@@ -42,9 +40,9 @@ public class DP_Back10844 {
 			if(memorization[n][l] != -1)
 				return memorization[n][l];
 			else {
-				long temp1 = Dynamic(n-1,l-1)%mod;
-				long temp2 = Dynamic(n-1,l+1)%mod;
-				return memorization[n][l] = (temp1 + temp2) % 1000000000;
+				int temp1 = Dynamic(n-1,l-1)%mod;
+				int temp2 = Dynamic(n-1,l-1)%mod;
+				return memorization[n][l] = temp1 + temp2;
 			}
 		}
 		else 
